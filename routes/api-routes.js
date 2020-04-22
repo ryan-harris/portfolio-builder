c47c441f44089ba0944088e350a5b42731001558
PinkFished <d.macias.apps@gmail.com>
2020-04-22 11:15:26 -0700

    Changed email to userName for sequilize row creation

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -14,12 +14,9 @@ module.exports = function(app) {
     });
   });
 
-  // Route for signing up a user. The user's password is automatically hashed and stored securely thanks to
-  // how we configured our Sequelize User Model. If the user is created successfully, proceed to log the user in,
-  // otherwise send back an error
   app.post("/api/signup", function(req, res) {
     db.User.create({
-      email: req.body.email,
+      userName: req.body.email,
       password: req.body.password
     })
       .then(function() {
