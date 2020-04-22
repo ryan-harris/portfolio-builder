5348420fd149775a6130b1a07833cde26601b378
Sorrab <ramonpbarros@gmail.com>
2020-04-22 11:40:53 -0700

    changed user model hasOne to settings

--- models/user.js
+++ models/user.js
@@ -34,7 +34,7 @@ module.exports = function(sequelize, DataTypes) {
         allowNull: false
       }
     });
-    models.User.hasOne(models.Repo, {
+    models.User.hasOne(models.Settings, {
       onDelete: "CASCADE",
       foreignKey: {
         allowNull: false
