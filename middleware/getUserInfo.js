db3f8f44285318ab7d9e90423a37a5da213375ff
PinkFished <d.macias.apps@gmail.com>
2020-04-22 20:48:41 -0700

    Added axios call for user repos

--- middleware/getUserInfo.js
+++ middleware/getUserInfo.js
@@ -2,6 +2,7 @@ const axios = require("axios");
 
 function getUserInfo(req, res, next) {
   const userData = {};
+  const repoList = [];
   // TODO: issue #47 get user info from database
   // will need join between repos and user information
 
@@ -9,15 +10,29 @@ function getUserInfo(req, res, next) {
 
   const userName = req.user.ghUsername;
 
-  axios.get("https://api.github.com/users/" + userName).then(response => {
-    userData.profileImg = response.data.avatar_url;
-    userData.about = response.data.bio;
-  });
-
-  // TODO: issue #45 get user info from github
+  axios
+    .get("https://api.github.com/users/" + userName)
+    .then(response => {
+      userData.name = response.data.name;
+      userData.profileImg = response.data.avatar_url;
+      userData.about = response.data.bio;
+      return axios.get("https://api.github.com/users/" + userName + "/repos");
+    })
+    .then(response => {
+      response.data.forEach(repo => {
+        const repoData = {
+          name: repo.name,
+          description: repo.description,
+          repoUrl: repo.html_url
+        };
+        repoList.push(repoData);
+      });
+      userData.repos = repoList;
+      console.log(userData);
+    });
 
   // assign the data here to pass along to next function
-  req.userData = "some data";
+  req.userData = userData;
 
   next();
 }
