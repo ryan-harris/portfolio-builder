22948eaf124d318c6e9a75ec3554756164ac15ae
PinkFished <d.macias.apps@gmail.com>
2020-04-22 21:23:42 -0700

    Created getUserRepo function

--- middleware/getUserInfo.js
+++ middleware/getUserInfo.js
@@ -1,23 +1,32 @@
 const axios = require("axios");
+const userData = {};
+const repoList = [];
 
 function getUserInfo(req, res, next) {
-  const userData = {};
-  const repoList = [];
   // TODO: issue #47 get user info from database
   // will need join between repos and user information
 
   // TODO: issue #46 get user repos from github
-
+  //req.user.ghUsername
   const userName = req.user.ghUsername;
 
+  axios.get("https://api.github.com/users/" + userName).then(response => {
+    userData.name = response.data.name;
+    userData.profileImg = response.data.avatar_url;
+    userData.about = response.data.bio;
+
+    getUserRepos(userName, () => {
+      req.userData = userData;
+      next();
+    });
+
+    // assign the data here to pass along to next function
+  });
+}
+
+function getUserRepos(userName, callback) {
   axios
-    .get("https://api.github.com/users/" + userName)
-    .then(response => {
-      userData.name = response.data.name;
-      userData.profileImg = response.data.avatar_url;
-      userData.about = response.data.bio;
-      return axios.get("https://api.github.com/users/" + userName + "/repos");
-    })
+    .get("https://api.github.com/users/" + userName + "/repos")
     .then(response => {
       response.data.forEach(repo => {
         const repoData = {
@@ -28,12 +37,8 @@ function getUserInfo(req, res, next) {
         repoList.push(repoData);
       });
       userData.repos = repoList;
+      callback();
     });
-
-  // assign the data here to pass along to next function
-  req.userData = userData;
-
-  next();
 }
 
 module.exports = getUserInfo;
