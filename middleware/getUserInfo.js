8a6fc9bf16ffe1040ac84e01d8bae4f939f88763
PinkFished <d.macias.apps@gmail.com>
2020-04-22 20:14:42 -0700

    Added axios request to get user data from github

--- middleware/getUserInfo.js
+++ middleware/getUserInfo.js
@@ -1,13 +1,24 @@
+const axios = require("axios");
+
 function getUserInfo(req, res, next) {
+  const userData = {};
   // TODO: issue #47 get user info from database
   // will need join between repos and user information
 
   // TODO: issue #46 get user repos from github
 
+  const userName = req.user.ghUsername;
+
+  axios.get("https://api.github.com/users/" + userName).then(response => {
+    userData.profileImg = response.data.avatar_url;
+    userData.about = response.data.bio;
+  });
+
   // TODO: issue #45 get user info from github
 
   // assign the data here to pass along to next function
   req.userData = "some data";
+
   next();
 }
 
