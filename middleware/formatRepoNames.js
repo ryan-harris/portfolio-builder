2ae6e478ed028fff36a21849fc46eff99d440937
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2020-04-26 12:42:05 -0700

    format title for filter functionality

--- middleware/formatRepoNames.js
+++ middleware/formatRepoNames.js
@@ -8,11 +8,20 @@ function formatRepoTitle(title) {
   return text;
 }
 
+function formatSearchName(title) {
+  const searchName = title
+    .toLowerCase()
+    .split("-")
+    .join("");
+  return searchName;
+}
+
 function formatRepoNames(repos) {
   const reposFormatted = repos.map(repo => {
     return {
       ...repo,
-      name: formatRepoTitle(repo.name)
+      name: formatRepoTitle(repo.name),
+      searchName: formatSearchName(repo.name)
     };
   });
 
